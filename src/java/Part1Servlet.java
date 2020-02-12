46bbb4166fed118d54974e3ef835fef0cfc4cdf2
798419 <798419@ELW9IT223137.ACDM.DS.SAIT.CA>
2020-02-12 10:36:21 -0700

    first commit

---
+++ src/java/Part1Servlet.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 798419
+ */
+public class Part1Servlet extends HttpServlet {
+
+    private int counter;
+
+    @Override
+    public void init() throws ServletException {
+        counter = 0;
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setAttribute("guestcount", "<p> you are visitor #" + counter + "!</p>");
+        counter++;
+        System.out.println("do GET");
+
+        //request.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        // response.sendRedirect("ShoppingList");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setAttribute("guestcount", "<p> you are visitor #" + counter + "!</p>");
+        counter++;
+        System.out.println("do Post");
+
+        //request.getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        // response.sendRedirect("ShoppingList");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/WEB-INF/part1.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : part1
+    Created on : Feb 12, 2020, 10:28:17 AM
+    Author     : 798419
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>CPRG352 Practice Exam 1</h1>
+
+
+
+
+        <hr>
+        ${guestcount}
+    </body>
+</html>
